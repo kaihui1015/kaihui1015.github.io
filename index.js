f9dedffb671c5ae4a14056c42abcf0e860903817
kaihui1015 <kaihuireg@gmail.com>
2021-04-05 13:03:20 +0800

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function transformUppercase(_val) {
     return _val.toUpperCase();
 }
 
-let elNamePengguna = document.getElementById("namaPengguna"); //select the element
-elNamePengguna.addEventListener("keyup", function () {
-    elNamePengguna.value = transformUppercase(elNamePengguna.value);
+let elNamaPengguna = document.getElementById("namaPengguna"); //select the element
+elNamaPengguna.addEventListener("keyup", function () {
+    elNamaPengguna.value = transformUppercase(elNamaPengguna.value);
 });
