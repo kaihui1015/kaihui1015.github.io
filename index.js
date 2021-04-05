6c467bf60dbdeb9cf232cc38cc2285e4813763dc
kaihui1015 <kaihuireg@gmail.com>
2021-04-05 13:13:15 +0800

    katalaluan

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ let elNamaPengguna = document.getElementById("namaPengguna"); //select the eleme
 elNamaPengguna.addEventListener("keyup", function () {
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value);
 });
+
+function checkPassword() {
+    let elKataLaluan = document.getElementById("KataLaluan");
+    let password_val = elKataLaluan.value;
+    if (password_val.length >= 5) {
+        alert("Password check ...OK!");
+    } else {
+        alert("Password too short!");
+    }
+}
+
+let elKataLaluan = document.getElementById("KataLaluan");
+elKataLaluan.onblur = checkPassword;
