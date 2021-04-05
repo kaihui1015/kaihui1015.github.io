b8e3682dbd94e54ce69fde5394b997d8b258550b
kaihui1015 <kaihuireg@gmail.com>
2021-04-05 22:11:32 +0800

    NewFolder1011

---
+++ event-handler2/js/index.js
@@ -0,0 +1,21 @@
+function transformUppercase(_val) {
+    return _val.toUpperCase();
+}
+
+let elNamaPengguna = document.getElementById("namaPengguna"); //select the element
+elNamaPengguna.addEventListener("keyup", function () {
+    elNamaPengguna.value = transformUppercase(elNamaPengguna.value);
+});
+
+function checkPassword(){
+    let elKataLaluan = document.getElementById("kataLaluan");
+    let password_val = elKataLaluan.value;
+    if (password_val.length > 5) {
+        alert("Password check ...OK!");
+    } else {
+        alert("Password too short!");
+    }
+}
+
+let elKataLaluan = document.getElementById("kataLaluan");
+elKataLaluan.onblur = checkPassword;
