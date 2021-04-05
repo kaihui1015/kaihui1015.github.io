5c252a42ccc60946d72c2ac1d7a9ed28a5a07dee
kaihui1015 <kaihuireg@gmail.com>
2021-04-05 20:45:41 +0800

    2045

--- video/js/index.js
+++ video/js/index.js
@@ -1,6 +1,11 @@
 let video = document.getElementById("myVideo")
 
-let playbtn = document.getElementById("playBtn")
+let playBtn = document.getElementById("playBtn")
 playBtn.addEventListener("click", function(){
     video.play();
+});
+
+let pauseBtn = document.getElementById("pauseBtn")
+playBtn.addEventListener("click", function(){
+    video.pause();
 });
