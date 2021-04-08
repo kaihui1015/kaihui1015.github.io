7cb53c8895a1bdff75a8806997d4b71e2028c493
kaihui1015 <kaihuireg@gmail.com>
2021-04-08 23:32:56 +0800

    1132

--- geolocation/js/haversine_main.js
+++ geolocation/js/haversine_main.js
@@ -1,7 +1,7 @@
 function findNearest(lat,lon){
-    let d1 = haversine(lat, lon, 2.922561,101.650965); //DePulze
-    let d2 = haversine(lat, lon, 3.073065,101.607787); //Sunway
-    let d3= haversine(lat, lon, 3.158761, 101.714524); //KLCC
+    let d1 = haversine (lat,lon,2.922561,101.650965); //DePulze
+    let d2 = haversine (lat, lon,3.073065,101.607787); //Sunway
+    let d3= haversine (lat,lon,3.158761,101.714524); //KLCC
 
     return [d1,d2,d3];
 }
