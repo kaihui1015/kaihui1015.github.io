2b4d9cb49bc09c7526f2ae3f1733678081399247
kaihui1015 <kaihuireg@gmail.com>
2021-04-12 19:23:26 +0800

    723

---
+++ tableapp/js/book.js
@@ -0,0 +1,18 @@
+let bookNowBtn = document.getElementById("bookNow");
+bookNowBtn.addEventListener("click", function(){
+    let userName = document.getElementById("userName");
+    let userNameVal = userName.Value;
+
+    let userEmail = document.getElementById("userEmail");
+    let userEmailVal = userEmail.Value;
+
+    //shorter form
+    let userPaxVal = document.getElementById("userPax").value;
+    let userRemarksVal = document.getElementById("userRemarks").value;
+
+    BookNow(userNameVal, userEmailVal, userPaxVal, userRemarks);
+});
+
+function BookNow(userName,userEmail,userPax,userRemarks){
+    alert(userName);
+}
