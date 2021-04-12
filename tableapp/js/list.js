3c1301e0044329ef77c5a18c99cddb354d594cc5
kaihui1015 <kaihuireg@gmail.com>
2021-04-12 18:52:23 +0800

    652

--- tableapp/js/list.js
+++ tableapp/js/list.js
@@ -1,5 +1,31 @@
 let refreshNowBtn = document.getElementById("refreshNow");
 refreshNowBtn.addEventListener("click",function(){
-    alert("GetBooking");
+    GetBooking();
 });
 
+function GetBooking(){
+    let url = 'https://api.sheety.co/50585a22aa4a35c43762829099ed20ab/tableapp/bookingItems';
+fetch(url)
+.then((response) => response.json())
+.then(json => {
+
+    let bookingNameList = document.getElementById("bookingNameList");
+
+    for(let i=0; i < json.bookingItems.length; i++){
+        let gName = json.bookingItems[i].name;
+        let gEmail = json.bookingItems[i].email;
+        let gPax = json.bookingItems[i].pax; 
+        let gRemarks = json.bookingItems[i].remarks;   
+        let gId = json.bookingItems[i].id;
+
+        let row = bookingNameList.insertRow(bookingNameList.row.length);
+        row.insertCell(0).innerHTML = gId;
+        row.insertCell(0).innerHTML = gName;
+        row.insertCell(0).innerHTML = gEmail;
+        row.insertCell(0).innerHTML = gPax;
+        row.insertCell(0).innerHTML = gRemarks;
+        row.insertCell(0).innerHTML = "TBC";
+
+    }  
+});
+}
