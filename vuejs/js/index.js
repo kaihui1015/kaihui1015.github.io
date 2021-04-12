533a2daa1471b942b359bf011bcf6cd3b27c9f50
kaihui1015 <kaihuireg@gmail.com>
2021-04-12 15:01:53 +0800

    301

--- vuejs/js/index.js
+++ vuejs/js/index.js
@@ -9,7 +9,7 @@ let app2 = new vue({
     el : "app2",
     data: {
         message :"loaded on " + new Date().toLocaleDateString(),
-        linkURL: "https//bbc.in/2LAnJs8",
+        linkURL: "https://www.malaysiakini.com/zh",
         linkText: "Google is down!",
     }
 });
