d3088277c7d334873c2390d520a4547a5bec9bca
kaihui1015 <kaihuireg@gmail.com>
2021-04-12 21:40:54 +0800

    0940

--- tableapp/js/list.js
+++ tableapp/js/list.js
@@ -29,7 +29,7 @@ fetch(url)
         row.insertCell(2).innerHTML = gEmail;
         row.insertCell(3).innerHTML = gPax;
         row.insertCell(4).innerHTML = gRemarks;
-        row.insertCell(5).innerHTML = "<button id='"+ gId "' class='btn btn-danger' > Delete </button>";
+        row.insertCell(5).innerHTML = "<button id='"+ gId + "' class='btn btn-danger' > Delete </button>";
 
     }  
 });
