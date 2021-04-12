41f971c9a31d9f46a90db5643a9aedf40f0d1eae
kaihui1015 <kaihuireg@gmail.com>
2021-04-12 20:31:03 +0800

    830

--- tableapp/js/book.js
+++ tableapp/js/book.js
@@ -27,6 +27,9 @@ function BookNow(userName,userEmail,userPax,userRemarks){
     fetch(url, {
       method: 'POST',
       body: JSON.stringify(body)
+      headers: {
+          "Content-Type": "application/json"
+      }
     })
     .then((response) => response.json())
     .then(json => {
