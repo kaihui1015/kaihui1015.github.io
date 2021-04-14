0168a6d923afd622bd07a21d8641d3791e0c73ff
kaihui1015 <kaihuireg@gmail.com>
2021-04-14 20:41:04 +0800

    842

--- tableapp/js/list.js
+++ tableapp/js/list.js
@@ -52,7 +52,6 @@ function Deletebooking(id) {
     fetch(url, {
         method: 'DELETE',
     })
-        .then((response) => response.json())
         .then(() => {
             alert("Record id " + id + " deleted!");
             GetBooking();
